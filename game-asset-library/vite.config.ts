9da538b73da1676969791bbdccfa75ef7b791354
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2025-07-14 18:36:07 -0700

    Update build files

--- game-asset-library/vite.config.ts
+++ game-asset-library/vite.config.ts
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/game-asset-library/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'three-js': ['three', '@react-three/fiber', '@react-three/drei']
+        }
+      }
+    }
+  }
 })
